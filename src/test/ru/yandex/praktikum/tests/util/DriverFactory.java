10330d3b4e81d91bfbd196362a739762f60d638f
Alan Kharaikyev <haraikyev.fg@yandex.com>
2025-12-12 03:13:27 +0300

    first commit

--- src/test/ru/yandex/praktikum/tests/util/DriverFactory.java
+++ src/test/ru/yandex/praktikum/tests/util/DriverFactory.java
@@ -2,44 +2,49 @@ package ru.yandex.praktikum.tests.util;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxOptions;
 
 import java.time.Duration;
 
 public class DriverFactory {
 
+    public static WebDriver createDriver() {
+        return createDriver("chrome");
+    }
+
     public static WebDriver createDriver(String browser) {
         WebDriver driver;
-        switch (browser.toLowerCase()) {
-            case "chrome":
-                ChromeOptions chromeOptions = new ChromeOptions();
-                // Закомментируйте headless для отладки
-                // chromeOptions.addArguments("--no-sandbox", "--headless", "--disable-dev-shm-usage");
-                chromeOptions.addArguments("--no-sandbox", "--disable-dev-shm-usage");
-                // Добавим настройки для лучшей видимости
-                chromeOptions.addArguments("--start-maximized");
-                chromeOptions.addArguments("--disable-blink-features=AutomationControlled");
-                chromeOptions.addArguments("--disable-infobars");
-                driver = new ChromeDriver(chromeOptions);
-                break;
 
-            case "firefox":
-                // Укажите путь к geckodriver
-                // Убедитесь, что путь указан правильно с расширением .exe для Windows
-                System.setProperty("webdriver.gecko.driver", "C:/Users/admin/Downloads/geckodriver-v0.36.0-win64/geckodriver.exe");
-                FirefoxOptions firefoxOptions = new FirefoxOptions();
-                firefoxOptions.addArguments("--start-maximized");
-                driver = new FirefoxDriver(firefoxOptions);
-                break;
+        // Всегда используем Chrome для единого окружения
+        if (!"chrome".equalsIgnoreCase(browser)) {
+            System.out.println("Внимание: Тесты настроены на запуск в Chrome. Браузер '" + browser + "' будет заменен на Chrome.");
+        }
 
-            default:
-                throw new IllegalArgumentException("Неподдерживаемый браузер: " + browser);
+        ChromeOptions chromeOptions = new ChromeOptions();
+
+        // Базовые настройки для стабильной работы
+        chromeOptions.addArguments("--no-sandbox");
+        chromeOptions.addArguments("--disable-dev-shm-usage");
+
+        // Настройки для лучшей видимости и стабильности
+        chromeOptions.addArguments("--start-maximized");
+        chromeOptions.addArguments("--disable-blink-features=AutomationControlled");
+        chromeOptions.addArguments("--disable-infobars");
+        chromeOptions.addArguments("--disable-extensions");
+        chromeOptions.addArguments("--disable-gpu");
+        chromeOptions.addArguments("--disable-notifications");
+
+        // Инициализация драйвера
+        try {
+            driver = new ChromeDriver(chromeOptions);
+        } catch (Exception e) {
+            throw new RuntimeException("Не удалось запустить Chrome. Убедитесь, что Chrome установлен: " + e.getMessage(), e);
         }
 
         // Установим неявное ожидание
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+
+
         driver.manage().window().maximize();
 
         return driver;
